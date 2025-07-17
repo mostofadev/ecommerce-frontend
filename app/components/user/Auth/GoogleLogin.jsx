bbd30fd3ab252734e8e2a8546091aef5a639ce8a
Mostofa kamal <125730272+Mostofakama@users.noreply.github.com>
2025-07-17 23:00:53 +0600

    Update GoogleLogin.jsx

--- app/components/user/Auth/GoogleLogin.jsx
+++ app/components/user/Auth/GoogleLogin.jsx
@@ -3,7 +3,7 @@ import { FcGoogle } from 'react-icons/fc';
 export default function GoogleLoginButton() {
   return (
     <a
-      href="http://mostofadev.com/api/auth/google/redirect"
+      href="https://mostofadev.com/api/auth/google/redirect"
       className="flex items-center justify-center gap-3 bg-white border border-gray-300 text-gray-700 hover:shadow-md hover:bg-gray-50 transition-all duration-200 px-5 py-2.5 rounded-full w-full max-w-sm mx-auto"
     >
       <FcGoogle className="text-xl" />
